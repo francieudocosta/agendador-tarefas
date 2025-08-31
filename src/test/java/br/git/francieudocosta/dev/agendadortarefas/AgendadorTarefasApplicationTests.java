0ccc0d54f526738234776e0279aea7a86349365b
francieudocosta <francieudocosta@gmail.com>
2025-08-31 16:27:27 -0300

    commit inicial com esqueleto

---
+++ src/test/java/br/git/francieudocosta/dev/agendadortarefas/AgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.git.francieudocosta.dev.agendadortarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTarefasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
